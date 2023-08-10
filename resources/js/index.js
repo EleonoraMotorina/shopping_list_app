8b1578cdf58389b40ba7216e13d7a9822765617a
motorinaeleonora@gmail.com <motorinaeleonora@gmail.com>
2023-08-10 13:56:47 +0300

    new changes

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,27 +1,21 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     const deleteButton = document.querySelectorAll('.delete-button');
-    
-//     deleteButton.forEach(button => {
-//         button.addEventListener('click', function(event) {
-//             event.preventDefault();
-            
-//             const itemId = button.getAttribute('data-item-id');
-            
-//             const confirmed = window.confirm("Are you sure you want to delete this item?");
-            
-//             if (confirmed) {
-              
-//                 const row = button.closest('tr');
-//                 if (row) {
-//                     row.remove();
-//                 }
-//             }
-//         });
-//     });
-// })
-
 document.addEventListener('DOMContentLoaded', function() {
-    const message = document.getElementById('message');
+    const deleteButton = document.querySelectorAll('.delete-button');
     
-    message.textContent = 'Привет, это сообщение через JavaScript!';
+    deleteButton.forEach(button => {
+        button.addEventListener('click', function(event) {
+            event.preventDefault();
+            
+            const itemId = button.getAttribute('data-item-id');
+            
+            const confirmed = window.confirm("Are you sure you want to delete this item?");
+            
+            if (confirmed) {
+
+                console.log(`Item with ID ${itemId} will be deleted.`);
+            } else {
+
+                console.log(`Deletion of item with ID ${itemId} was canceled.`);
+            }
+        });
+    });
 });
